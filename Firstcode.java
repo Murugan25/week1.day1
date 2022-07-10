3730c5ba1479408d83fc85da707d820f54d79964
Murugan25 <109011324+Murugan25@users.noreply.github.com>
2022-07-10 09:41:30 +0530

    Add files via upload

---
+++ Firstcode.java
@@ -0,0 +1,10 @@
+package week1.day1;
+
+public class Firstcode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Test world");
+	}
+
+}
---
+++ OddorEven.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class OddorEven {
+
+	public static void main(String[] args) {
+	int i= 9;
+			if(i % 2==0)
+			{
+				System.out.println("It is Even");
+			}
+			else
+				
+			{
+				System.out.println("It is Odd");
+			}
+}
+
+	}
+
+
